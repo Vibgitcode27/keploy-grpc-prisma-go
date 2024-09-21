be5834bc01150845908ad934e614686a7681fe1a
vibhor <vibgitcode27@gmail.com>
2024-09-22 01:09:46 +0530

    added client code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"krpg/krpg"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 func main() {
-	fmt.Println("Hello World from Client")
+	serverAddress := flag.String("address", "", "The server address in the format of host:port")
+	flag.Parse()
+	fmt.Printf("Dial server on address %s", *serverAddress)
+
+	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal("Error dialing server: ", err)
+	}
+
+	todoClient := krpg.NewTodoServiceClient(conn)
+	req := &krpg.CreateRequest{
+		Title:       "Learn gRPC",
+		Description: "Learn how to use gRPC in Go",
+		DueDate:     "2021-09-01",
+	}
+
+	res, err := todoClient.Create(context.Background(), req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && st.Code() == codes.AlreadyExists {
+			fmt.Println("Laptop already exists")
+		} else {
+			log.Fatal("Cannot create laptop(Response)", err)
+		}
+		return
+	}
+
+	fmt.Printf("Todo created with id: %s", res.Task.Id)
+
 }
--- makefile
+++ makefile
@@ -2,7 +2,13 @@ gen:
 	protoc --go_out=. --go-grpc_out=. proto/*.proto
 
 server:
-	go run cmd/server/main.go -port 8080
+	go run cmd/server/main.go -port 8000
 
 client:
-	go run cmd/client/main.go
+	go run cmd/client/main.go -address 0.0.0.0:8000
+
+keploy:
+	GOPATH=$(HOME)/go keploy record -c "/usr/local/go/bin/go run cmd/server/main.go -port 8000"
+
+start:
+	sudo -E make keploy
